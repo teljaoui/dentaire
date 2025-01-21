30b5e0d8b8815ba7ef39c0f35b4855d627d7d9ed
teljaoui <teljaoui2004@gmail.com>
2025-01-21 11:04:11 +0100

    up

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,41 @@
+<header>
+    <nav class="header-top">
+        <div>
+            <ul>
+                <li><a href=""><i class="fa-solid fa-location-dot info"></i>info@gmail.com</a></li>
+                <li><a href=""><i class="fa-solid fa-envelope info"></i>Test , Test , N00</a></li>
+            </ul>
+            <ul class="media">
+                <li><a href=""><i class="fa-solid fa-phone"></i></a></li>
+                <li><a href=""><i class="fa-brands fa-whatsapp"></i></a></li>
+                <li><a href=""><i class="fa-brands fa-facebook"></i></a></li>
+                <li><a href=""><i class="fa-brands fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </nav>
+    <nav class="header-bottom">
+        <div>
+            <ul>
+                <li class="logo">
+                    <a href="/"><img src="img/logo.png"
+                            alt=""srcset=""><span>Dentaire</span></a>
+                            <button><i class="fa-solid fa-bars"></i></button>
+                </li>
+            </ul>
+            <ul class="echo "# dentaire" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/teljaoui/dentaire.git
+git push -u origin main">
+                <li><a href="/" class="navigation">Accueil</a></li>
+                <li><a href="/#services" class="navigation">Services</a></li>
+                <li><a href="/#home-contact" class="navigation">Contact</a></li>
+            </ul>
+            <ul class="phonehidden">
+                <li class="reserve"><a href="/appointment">Réserver</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
